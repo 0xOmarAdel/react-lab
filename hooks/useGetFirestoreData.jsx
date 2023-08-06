e822e535766aa20499bcfd659d46fca99763eedc
Omar Adel <omaradelawad20013@gmail.com>
2023-08-06 05:27:35 +0300

    .

---
+++ hooks/useGetFirestoreData.jsx
@@ -0,0 +1,70 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+import {doc, getDoc, getDocs, collection, where, query, limit, orderBy} from 'firebase/firestore';
+import {db} from '../firebase';
+
+ const useGetFirestoreData = (c, documentId, whereStatement, orderBy_, orderType, limit_) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const whereStatementRef  = useRef(whereStatement);
+
+  const fetchData = useCallback( async () => {
+    try {
+      setIsLoading(true);
+      if (documentId) {
+        const docRef = doc(db, c, documentId);
+        const docSnap = await getDoc(docRef)
+        if (docSnap.exists()) {
+          setData(docSnap.data());
+        }
+      } else {
+        let queryCollection = collection(db, c);
+
+        let queryWhere;
+        if (whereStatementRef.current) {
+          queryWhere = 
+          whereStatementRef.current?.lhs && whereStatementRef.current?.op && whereStatementRef.current?.rhs &&
+          where(whereStatementRef.current.lhs , whereStatementRef.current.op, whereStatementRef.current.rhs);
+        }
+
+        let queryOrderBy = orderBy_ && orderBy(orderBy_, orderType || 'asc');
+        let queryLimit = limit_ && limit(limit_);
+
+        const queryConstraints = {queryWhere, queryOrderBy, queryLimit}
+        const filteredQueryConstraints = Object.entries(queryConstraints).reduce((acc, [key, value]) => {
+          if (value !== null && value !== undefined) {
+            acc[key] = value;
+          }
+          return acc;
+        }, {});
+        const filteredQueryConstraintsArray = Object.values(filteredQueryConstraints);
+
+        let result = [];
+
+        const querySnapshot = await getDocs(filteredQueryConstraints ? query(queryCollection, ...filteredQueryConstraintsArray) : queryCollection);
+
+        querySnapshot.forEach((doc) => {
+          result.push({ ...doc.data(), id: doc.id });
+        });
+
+        setData(result);
+      }
+      setIsLoading(false);
+    } catch (error) {
+      setError(error)
+    }
+  }, [c, documentId, limit_, orderBy_, orderType]);
+
+  useEffect(() => {
+     fetchData();
+  }, [fetchData]);
+
+  useEffect(() => {
+    whereStatementRef.current = whereStatement;
+  }, [whereStatement]);
+  
+  return { data, isLoading, error };
+};
+
+ export default useGetFirestoreData;
